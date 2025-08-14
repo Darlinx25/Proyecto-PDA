aa846be9e63347c289583b0531c3e9c5c090eb21
Darlinx25 <fsalaberry505@gmail.com>
2025-08-14 17:54:08 -0300

    Primer commit

---
+++ src/main/java/com/mycompany/proyecto_pda/Proyecto_PDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_pda;
+
+/**
+ *
+ * @author faxcundo
+ */
+public class Proyecto_PDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
