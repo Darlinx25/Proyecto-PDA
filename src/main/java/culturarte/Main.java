b50ae7a7f7ddc7596bf1e0d9c62207bb3f3a05e2
Darlinx25 <fsalaberry505@gmail.com>
2025-08-14 22:17:32 -0300

    a ver

--- src/main/java/culturarte/Main.java
+++ src/main/java/culturarte/Main.java
@@ -14,5 +14,6 @@ public class Main {
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
         frame.setVisible(true);
+        System.out.println("xd");
     }
 }
