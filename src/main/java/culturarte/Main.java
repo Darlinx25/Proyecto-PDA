87dead29b26668ebad09a48cd19f8e38e2f2335f
Darlinx25 <fsalaberry505@gmail.com>
2025-08-14 21:37:23 -0300

    orden

--- src/main/java/culturarte/Main.java
+++ src/main/java/culturarte/Main.java
@@ -2,17 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.proyecto_pda;
-
+package culturarte;
+import javax.swing.*;
+import culturarte.graficos.MainFrame;
 /**
  *
  * @author faxcundo
  */
-public class Proyecto_PDA {
+public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("JEJEJE");
-        
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
     }
 }
