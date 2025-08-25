aedd9c963f6f561076a193a570322a74db33ce61
Faxcundo <fsalaberry505@gmail.com>
2025-08-25 00:09:54 -0300

    Eliminar carpeta target del repo y agregar al .gitignore

--- src/main/java/culturarte/Main.java
+++ src/main/java/culturarte/Main.java
@@ -14,9 +14,7 @@ public class Main {
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
         frame.setVisible(true);
-        System.out.println("xd");
-        System.out.println("no compila");
-        System.out.println("llevo como 2-3 horas trabajando en este proyecto y recien escribo algo de codigo lol matenme");
+   
         
     }
 }
