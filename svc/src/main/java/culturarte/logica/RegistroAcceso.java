0e18f247ae78dfaef3eaee9b80beb20166a88f0d
bot0014 <markgitjub@outlook.com>
2025-10-29 19:56:20 -0300

    RegistroAccesso clase

---
+++ svc/src/main/java/culturarte/logica/RegistroAcceso.java
@@ -0,0 +1,72 @@
+
+package culturarte.logica;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "accesos")
+public class RegistroAcceso {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+    private String ip;
+    private String url;
+    private String browser;
+    private String os;
+
+    public RegistroAcceso() {}
+    
+    public RegistroAcceso(Long id, String ip, String url, String browser, String os) {
+        this.id = id;
+        this.ip = ip;
+        this.url = url;
+        this.browser = browser;
+        this.os = os;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+    
+}
